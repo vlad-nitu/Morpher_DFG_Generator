e3da3125ad4031704253119fbdbc667c5e575de8
Vlad-Petru Nitu <vlnitu@student.ethz.ch>
2025-07-07 14:20:32 +0200

    fix(vm): revert 4radix correct + fix bug in int main

--- benchmarks/morpher_benchmarks/4radix/radix4.c
+++ benchmarks/morpher_benchmarks/4radix/radix4.c
@@ -1,40 +1,103 @@
-#include <string.h>
 #include <stdio.h>
+#include <stdint.h>
 
+#define ENTRIES 512
+#define PAGE_SHIFT 12
+#define LEVELS 4
 
-#define SIZE  20
-int A[SIZE], B[SIZE], C[SIZE];
+// !!!!!!!!!!!! If we define DEBUG and then use CGRA compiler to map, it will see the printf and will translate it into the 'call' assembly instruction
+// which Morpher does not support (i.e, doesn't know how to map it).
+// So, when compiling with CGRA_COMPILER, MAKE SURE THIS MACRO IS UNDEFINED: DEBUG.
+#undef DEBUG
 
-__attribute__((noinline))
-void page_table_walk(){
+typedef uint64_t u64;
 
-   
-   for (int i=0;i<SIZE; i++){
-      #ifdef CGRA_COMPILER
-      please_map_me();
-      #endif
+// Simulated page tables
+u64 PML4[ENTRIES];
+u64 PDPT[ENTRIES];
+u64 PD[ENTRIES];
+u64 PT[ENTRIES]; 
 
-      C[i] += A[i]+B[i];
-   }
+// Simulated physical frames (for illustration)
+u64 physical_memory[1024];
 
+// VA global variabel, the LLVM optimiser (opt) expects a function w/o args?
+u64 va = -1;
+u64 pa = -1;
 
+// Helper to extract index bits from a virtual address
+int get_index(u64 va, int level) {
+    return (va >> (PAGE_SHIFT + 9 * level)) & 0x1FF;  // 9 bits per level
 }
 
-int main(){
+// Perform a simulated page table walk
+void page_table_walk() {
 
-int i,j;
+    u64* tables[4] = {PT, PD, PDPT, PML4};  // Bottom-up order
+    int indexes[4];
+    
+    indexes[3] = get_index(va, 0);  // PML4 index
+    indexes[2] = get_index(va, 1);  // PDPT index
+    indexes[1] = get_index(va, 2);  // PD index
+    indexes[0] = get_index(va, 3);  // PT index
 
-for (i=0;i<SIZE; i++){
-      A[i] = i * 2 + 5;
-      B[i] = i * 3;
-      C[i] = 0;
+#ifdef DEBUG
+    printf("VA: 0x%lx -> Indexes: [%d, %d, %d, %d]\n",
+           va, indexes[3], indexes[2], indexes[1], indexes[0]);
+#endif
+
+    // Page table walk using while loop
+    u64* current_table = PML4;
+    for (int level = 3; level > 0; level--) {
+#ifdef CGRA_COMPILER
+    please_map_me();
+#endif
+        int idx = indexes[level];
+        if (current_table[idx] == 0) {
+#ifdef DEBUG
+            printf("Level %d miss\n", level);
+#endif
+            return;
+        }
+        current_table = (u64*)current_table[idx];
+        level--;
     }
-    
-page_table_walk();
 
-for (i=0;i<SIZE; i++) printf("%d\n", C[i]);
-    
+    // PT level
+    int pt_idx = indexes[0];
+    if (current_table[pt_idx] == 0) {
+#ifdef DEBUG
+        printf("PT miss\n");
+#endif
+        return;
+    }
 
-return 0;
+    u64 page_offset = va & 0xFFF;
+    u64 frame_base = current_table[pt_idx];
+    // pa = frame_base + page_offset;
+}
 
+int main() {
+    // Setup a virtual-to-physical mapping
+    va = 0x123456789;  // Example virtual address
+
+    // Setup simulated page table entries
+    int pml4_idx = get_index(va, 3);
+    int pdpt_idx = get_index(va, 2);
+    int pd_idx   = get_index(va, 1);
+    int pt_idx   = get_index(va, 0);
+
+    PML4[pml4_idx] = (u64)&PDPT[0];
+    PDPT[pdpt_idx] = (u64)&PD[0];
+    PD[pd_idx] = (u64)&PT[0];
+    PT[pt_idx] = (u64)&physical_memory[0];  // Points to physical frame
+
+    page_table_walk();
+
+    if (pa)
+        printf("Translated PA: 0x%lx\n", pa);
+    else
+        printf("Translation failed.\n");
+
+    return 0;
 }

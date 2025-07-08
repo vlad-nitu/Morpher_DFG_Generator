403d84b3fc4941fedcd51772a7cc39411b73bb0f
Vlad-Petru Nitu <vlnitu@student.ethz.ch>
2025-07-08 12:20:47 +0200

    feat: compelx shifting to get compiler confused, and thus not optimise away my computation

--- benchmarks/morpher_benchmarks/4radix/radix4.c
+++ benchmarks/morpher_benchmarks/4radix/radix4.c
@@ -20,7 +20,7 @@ void page_table_walk(void)
         please_map_me();                  /* exactly one token */
 #endif
         u32 x = PTBL[cur_lvl][ idx[cur_lvl] ];       /* pure 32-bit GEP + load/store */
-        PTBL[LVLS - 1][ ENTRIES - 1 ] = ((x << 16) + 0xab) & 0xff;  // Perform a dummy store + index calculation operation
+        PTBL[LVLS - 1][ ENTRIES - 1 ] = ((x << 4) + 0xab) & 0xffffffff;  // Perform a dummy store + index calculation operation
     }
 }
 

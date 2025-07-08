8b6e199636178a23d642c1e0da3eb3c20e0e866d
Vlad-Petru Nitu <vlnitu@student.ethz.ch>
2025-07-08 12:15:28 +0200

    feat: dummy store to not opt away PTBL

--- benchmarks/morpher_benchmarks/4radix/radix4.c
+++ benchmarks/morpher_benchmarks/4radix/radix4.c
@@ -21,6 +21,7 @@ void page_table_walk(void)
 #endif
         u32 x = PTBL[cur_lvl][ idx[cur_lvl] ];       /* pure 32-bit GEP + load/store */
         x = (x << 16) & 0xff; // Perform a dummy index calculation operation
+        PTBL[LVLS - 1][ENTRIES - 1] = x; // Dummy store to not optimise away PTBL
     }
 }
 
